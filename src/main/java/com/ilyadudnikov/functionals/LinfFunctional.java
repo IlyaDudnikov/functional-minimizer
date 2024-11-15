13e12e0a0345a8624a664d5825b9233de31ef55d
ilyad <iladudnikov13@gmail.com>
2024-11-15 20:11:14 +0700

    fix: fixed LinfFunctional

--- src/main/java/com/ilyadudnikov/functionals/LinfFunctional.java
+++ src/main/java/com/ilyadudnikov/functionals/LinfFunctional.java
@@ -6,10 +6,8 @@ import com.ilyadudnikov.math.IVector;
 import java.util.List;
 
 public class LinfFunctional implements IFunctional {
-    private List<IVector> points;
-    private List<Double> funcPoints;
-
-    public LinfFunctional() {}
+    private final List<IVector> points;
+    private final List<Double> funcPoints;
 
     public LinfFunctional(List<IVector> points, List<Double> funcPoints) {
         this.points = points;
@@ -20,9 +18,9 @@ public class LinfFunctional implements IFunctional {
     public double value(IFunction function) {
         double sup = Math.abs(function.value(points.get(0)) - funcPoints.get(0));
         for (int i = 1; i < points.size(); i++) {
-            double curSup = Math.abs(function.value(points.get(i)) - funcPoints.get(i));
-            if (curSup > sup) {
-                sup = curSup;
+            double curDifference = Math.abs(function.value(points.get(i)) - funcPoints.get(i));
+            if (curDifference > sup) {
+                sup = curDifference;
             }
         }
         return sup;
