2c6222c70d7bc0de27a5623bc2cb015e37a561e3
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-11-15 12:52:56 +0300

    refactor: refactor PiecewiseLinearFunction

--- src/main/java/com/ilyadudnikov/functions/PiecewiseLinearFunction.java
+++ src/main/java/com/ilyadudnikov/functions/PiecewiseLinearFunction.java
@@ -4,15 +4,11 @@ import com.ilyadudnikov.math.IVector;
 import com.ilyadudnikov.math.Vector;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PiecewiseLinearFunction implements IParametricFunction, IDifferentiableFunction {
-    private IVector parameters;
-    List<Double> x =  new ArrayList<>(Arrays.asList(0.0, 1.0, 2.0, 3.0, 4.0));
+    List<Double> x;
     List<Double> q;
-
-    public PiecewiseLinearFunction() {}
 
     public PiecewiseLinearFunction(IVector parameters) {
         bind(parameters);
@@ -20,57 +16,67 @@ public class PiecewiseLinearFunction implements IParametricFunction, IDifferenti
 
     @Override
     public IVector gradient(IVector point) {
-        IVector res = new Vector();
+        int intervalIndex = findIntervalIndex(point.get(0));
+        if (intervalIndex == -1) {
+            throw new IllegalArgumentException("Point is outside the defined intervals");
+        }
 
-        int iCur = 0;
-        double p = point.get(0);
+        IVector gradientVector = initializeGradientVector();
+        applyBasisFunctions(gradientVector, point.get(0), intervalIndex);
+
+        return gradientVector;
+    }
+
+    private int findIntervalIndex(double p) {
         for (int i = 0; i < x.size() - 1; i++) {
-            if (p == x.get(i) || p == x.get(i + 1)) {
-                return res;
-            }
-            if (p > x.get(i) && p < x.get(i + 1)) {
-                iCur = i;
-                break;
+            if (p >= x.get(i) && p <= x.get(i + 1)) {
+                return i;
             }
         }
+        return -1;
+    }
 
-        for (int i = 0; i < q.size(); i++) {
-            res.add(0.0);
-        }
+    private IVector initializeGradientVector() {
+        Vector gradientVector = new Vector();
+        gradientVector.setSize(q.size());
+        return gradientVector;
+    }
 
-        double hx = (x.get(iCur + 1) - x.get(iCur));
-        double ksi = (p - x.get(iCur)) / hx;
-        double psi1 = 1 - ksi;
-        double psi2 = ksi;
+    private void applyBasisFunctions(IVector gradientVector, double p, int intervalIndex) {
+        double intervalLength = x.get(intervalIndex + 1) - x.get(intervalIndex);
+        double normalizedPosition = (p - x.get(intervalIndex)) / intervalLength;
 
-        res.set(iCur, psi1);
-        res.set(iCur + 1, psi2);
+        double basisFunction1 = 1 - normalizedPosition;
 
-        return res;
+        gradientVector.set(intervalIndex, basisFunction1);
+        gradientVector.set(intervalIndex + 1, normalizedPosition);
     }
 
     @Override
     public double value(IVector point) {
+        double px = point.get(0);
+
+        int iCur = getIntervalIndexByPoint(px);
+
+        double hx = (x.get(iCur + 1) - x.get(iCur));
+        double ksi = (px - x.get(iCur)) / hx;
+        double psi1 = 1 - ksi;
+        return q.get(iCur) * psi1 + q.get(iCur + 1) * ksi;
+    }
+
+    private int getIntervalIndexByPoint(double p) {
         int iCur = 0;
-        double p = point.get(0);
         for (int i = 0; i < x.size() - 1; i++) {
             if (p >= x.get(i) && p <= x.get(i + 1)) {
                 iCur = i;
                 break;
             }
         }
-        double hx = (x.get(iCur + 1) - x.get(iCur));
-        double ksi = (p - x.get(iCur)) / hx;
-        double psi1 = 1 - ksi;
-        double psi2 = ksi;
-
-        double func = q.get(iCur) * psi1 + q.get(iCur + 1) * psi2;
-        return func;
+        return iCur;
     }
 
     @Override
     public IFunction bind(IVector parameters) {
-        this.parameters = parameters;
         q = new ArrayList<>();
         q.addAll(parameters);
         return this;
